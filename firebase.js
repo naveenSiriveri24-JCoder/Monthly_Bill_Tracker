9474067dcec1586d995d5167052de6116c548289
naveenSiriveri24-JCoder <naveensiriveri.24@gmail.com>
2026-07-01 21:03:21 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration values.

--- firebase.js
+++ firebase.js
@@ -13,22 +13,22 @@ from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 const firebaseConfig = {
 
     apiKey:
-        "AIzaSyBV5xffgdbeQ4R6wuQa3VC97Pg4dI_jFDk",
+        "",
 
     authDomain:
-        "expense-tracker-6412b.firebaseapp.com",
+        "",
 
     projectId:
-        "expense-tracker-6412b",
+        "",
 
     storageBucket:
-        "expense-tracker-6412b.firebasestorage.app",
+        "",
 
     messagingSenderId:
-        "652987652417",
+        "",
 
     appId:
-        "1:652987652417:web:ed36317e16a965c057a9c0"
+        ""
 
 };
 
